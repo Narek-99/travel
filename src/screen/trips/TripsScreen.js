d8d6d375cc0fa49c75053d9e1f1e8d33f8025f0b
Narek-99 <56992961+Narek-99@users.noreply.github.com>
2025-04-06 19:47:09 +0200

    fixed

--- src/screen/trips/TripsScreen.js
+++ src/screen/trips/TripsScreen.js
@@ -271,7 +271,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   headerActions: { flexDirection: 'col', alignItems: 'center', gap: wp(3) },
-  contentContainer: { flex: 1, padding: 10 },
+  contentContainer: { flex: 1, padding: 20 },
   emptyText: {
     color: COLOR.black,
     textAlign: 'center',
