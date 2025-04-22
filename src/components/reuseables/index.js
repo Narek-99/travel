359f52c7a0f03b245af96b4e10a94c2267fe4cbb
Narek-99 <56992961+Narek-99@users.noreply.github.com>
2025-04-22 21:25:06 +0200

    fixed removed components

--- src/components/reuseables/index.js
+++ src/components/reuseables/index.js
@@ -1,21 +1,15 @@
 import AppHeader from "./headers";
-import Input from "./input";
 import Label from "./label";
 import Photo from "./photo";
 import Pressable from "./pressable";
-import If from "./if";
-import SvgItem from "./svgItem";
 import Button from "./button";
 import Scrollable from "./scrollable";
 
 export {
     AppHeader,
-    Input,
     Label,
     Photo,
     Pressable,
-    If,
-    SvgItem,
     Button,
     Scrollable
 }
--- src/components/reuseables/svgItem/index.js
+++
@@ -1,3 +0,0 @@
-import SvgItem from "./SvgItem";
-
-export default SvgItem
