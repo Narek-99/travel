701bf5c020620eccfc5953b3f4d333632d8158df
Narek-99 <56992961+Narek-99@users.noreply.github.com>
2025-05-11 10:20:38 +0200

    added images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,15 @@
 const Onboarding1 = require('./onboarding1.png')
 const Heart = require('./heart.png')
+const Paris = require('./paris.jpg')
+const Bangkok = require('./bangkok.jpg')
+const NewYork = require('./newyork.jpg')
+const Tokyo = require('./tokyo.jpg')
 
 export const IMAGES = {
     Onboarding1,
-    Heart
+    Heart,
+    Paris,
+    Bangkok,
+    NewYork,
+    Tokyo
 }
